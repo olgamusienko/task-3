cead24343f12cd7c41fe3ba91629b59d97c9cde8
olgamusienko <ogamusienko1@gmail.com>
2023-06-02 17:49:09 +0200

    Added a solution of pool area calculating

---
+++ src/main/java/Rectangle.java
@@ -0,0 +1,36 @@
+public class Rectangle {
+    private double width, length;
+
+    public Rectangle(double width, double length) {
+        this.width = width;
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setWidth(double width) {
+        if(width < 0){
+            this.width = 0;
+        } else {
+            this.width = width;
+        }
+    }
+
+    public void setLength(double length) {
+        if(length < 0){
+            this.length = 0;
+        } else {
+            this.length = length;
+        }
+    }
+
+    public double getArea(){
+        return width * length;
+    }
+}
